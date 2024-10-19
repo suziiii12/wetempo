dc2222590ccd6786c182a95f0365967e8e42fb8a
Sooji Lee <ac.s00jii1219@gmail.com>
2024-10-19 14:10:58 -0400

    Add wetempo project files

---
+++ atempo/main/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Post
+
+# 관리자(admin)가 게시글(Post)에 접근 가능
+admin.site.register(Post)
---
+++ atempo/main/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# 게시글(Post)엔 제목(postname), 내용(contents)이 존재합니다
+class Post(models.Model):
+    postname = models.CharField(max_length=50)
+    contents = models.TextField()
